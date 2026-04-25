3d6cc4d140fdcb60e55d473a9080f8d1dbc3b851
oi-ulu <275747227+Oi-Ulu@users.noreply.github.com>
2026-04-25 09:33:31 -0500

    arreglo de rutas de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Para desarrollo local: http://localhost:4321 funciona con base.
-  // En producción (GitHub Pages) se usará https://oi-ulu.github.io
   site: 'https://oi-ulu.github.io',   // ← dominio raíz de GitHub Pages
   base: '/mina-origen',
-  // integrations: [tailwind()], // Comentado porque no está instalado @astrojs/tailwind
 });
